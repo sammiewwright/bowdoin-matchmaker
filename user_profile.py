3fc715c36d636ee447247753980e303d9a74feea
sammiewwright <samanthaww@live.com>
2026-05-03 15:08:48 -0400

    Update user_profile.py

--- user_profile.py
+++ user_profile.py
@@ -5,9 +5,11 @@ class Person:
         self.age = int(age)
 
 class Profile(Person):
-    def __init__(self, first_name, last_name, age, interest1, interest2, music_type, hometown, goal):
+    def __init__(self, first_name, last_name, age, gender, preferred_gender, interest1, interest2, music_type, hometown, goal):
         super().__init__(first_name, last_name, age)
         self.last_name = last_name
+        self.gender = gender
+        self.preferred_gender = preferred_gender
         self.interest1 = interest1
         self.interest2 = interest2
         self.music_type = music_type
@@ -16,3 +18,4 @@ class Profile(Person):
 
     def get_interests(self):
         return {self.interest1, self.interest2}
+    
